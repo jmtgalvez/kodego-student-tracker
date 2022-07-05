d635086dee646c9ee6cfbcf1bd937b64104168ae
jmtgalvez <jmtgalvez@gmail.com>
2022-07-05 10:02:07 +0800

    Fixed connection option from 'hostname' to 'host'.

--- routes/database/index.js
+++ routes/database/index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  hostname: process.env.DATABASE_HOSTNAME,
+  host: process.env.DATABASE_HOSTNAME,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE
